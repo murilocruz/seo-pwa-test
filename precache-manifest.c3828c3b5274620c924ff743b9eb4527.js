23e3fd8b9e9c25f2ce918f941294e41c46440380
Murilo Cruz <murilo.cruz@i-value.com.br>
2020-03-02 15:24:33 -0300

    Updates

---
+++ precache-manifest.c3828c3b5274620c924ff743b9eb4527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea8b8cfdb86c4057fd0a303cd13a1c6",
+    "url": "/murilocruz/seo-pwa-test/index.html"
+  },
+  {
+    "revision": "861458d7521cc97c1411",
+    "url": "/murilocruz/seo-pwa-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a373a62911f0cfa61cee",
+    "url": "/murilocruz/seo-pwa-test/static/js/2.baea63d9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/murilocruz/seo-pwa-test/static/js/2.baea63d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861458d7521cc97c1411",
+    "url": "/murilocruz/seo-pwa-test/static/js/main.9b98685a.chunk.js"
+  },
+  {
+    "revision": "4b4ae174e007f2a72b0a",
+    "url": "/murilocruz/seo-pwa-test/static/js/runtime-main.a6e9953c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/murilocruz/seo-pwa-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
